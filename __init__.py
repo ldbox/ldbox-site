eea87cb2646867800d8e290e2a861082cc66c7b2
Oleg Girko <ol@infoserver.lv>
2015-09-23 18:50:26 +0100

    License the whole package under AGPL 3.
    Added COPYING file with AGPL 3.0 text.
    Added copyright notice to all files.
    
    Signed-off-by: Oleg Girko <ol@infoserver.lv>

---
+++ __init__.py
@@ -0,0 +1,17 @@
+# Copyright 2015 Oleg Girko <ol@infoserver.lv>
+#
+# This file is part of ldbox.site Python package.
+#
+# ldbox.site Python package is free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
