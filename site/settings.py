ed44a49aeddf12bd0d379e0c271a2adeea8a5855
Oleg Girko <ol@infoserver.lv>
2018-05-02 04:23:46 +0100

    Update to Django 2.0.
    * Using MIDDLEWARE setting instead of MIDDLEWARE_CLASSES.

--- site/settings.py
+++ site/settings.py
@@ -74,7 +74,7 @@ INSTALLED_APPS = (
     'waliki.togetherjs',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
